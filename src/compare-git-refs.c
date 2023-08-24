f75fc952fda27e6515c9d7f2ecd022979dae6dd9
Mattias Jansson <fimblo@yanson.org>
2023-08-24 17:45:43 +0200

    feat: compare local and remote refs
    Stand-alone helper program to figure out how to check if the local and
    remote tracking branch are the same or not.

--- src/compare-git-refs.c
+++ src/compare-git-refs.c
@@ -6,41 +6,103 @@ int main() {
     git_libgit2_init();
 
     git_repository *repo = NULL;
-    git_repository_open(&repo, ".");
-
-    // Get the current branch name
-    const char *your_branch_name = NULL;
-    git_reference *head_ref = NULL;
-    git_repository_head(&head_ref, repo);
-    your_branch_name = git_reference_shorthand(head_ref);
-
-    // Get the remote tracking branch name (e.g., origin/main)
-    const char *remote_tracking_branch_name = NULL;
-    git_reference *upstream_ref = NULL;
-    git_reference_lookup(&upstream_ref, repo, "HEAD");
-    const git_oid *upstream_target = git_reference_target(upstream_ref);
-    remote_tracking_branch_name = git_oid_tostr_s(upstream_target) + strlen("refs/remotes/");
-
-    git_reference *your_branch_ref = NULL;
-    git_reference_lookup(&your_branch_ref, repo, your_branch_name);
-
-    git_reference *remote_branch_ref = NULL;
-    git_reference_lookup(&remote_branch_ref, repo, remote_tracking_branch_name);
-
-    int cmp_result = git_reference_cmp(your_branch_ref, remote_branch_ref);
-
-    if (cmp_result != 0) {
-        printf("Your branch is different from the remote tracking branch.\n");
-    } else {
-        printf("Your branch is the same as the remote tracking branch.\n");
+    int error = git_repository_open(&repo, ".");
+    if (error != 0) {
+        printf("Failed to open repository.\n");
+        git_libgit2_shutdown();
+        return 1;
     }
 
-    git_reference_free(head_ref);
-    git_reference_free(upstream_ref);
-    git_reference_free(your_branch_ref);
-    git_reference_free(remote_branch_ref);
-    git_repository_free(repo);
+    // Local branch stuff //////////////////////////////////////////////////
 
+    git_reference *head_ref = NULL;
+    error = git_repository_head(&head_ref, repo);
+    if (error != 0) {
+        printf("Failed to get HEAD reference.\n");
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return 1;
+    }
+
+    char local_branch_name[128];
+    sprintf(local_branch_name, "refs/heads/%s",  git_reference_shorthand(head_ref));
+
+    
+    git_reference *local_branch_ref = NULL;
+    error = git_reference_lookup(&local_branch_ref, repo, local_branch_name);
+    if (error != 0) {
+        printf("Failed to lookup local branch reference.\n");
+        git_reference_free(head_ref);
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return 1;
+    }
+
+    const git_oid *local_branch_commit_id = git_reference_target(local_branch_ref);
+
+
+
+    // Upstream branch stuff //////////////////////////////////////////////////
+    char full_remote_branch_name[128];
+    sprintf(full_remote_branch_name, "refs/remotes/origin/%s", git_reference_shorthand(head_ref));
+
+
+    git_reference *upstream_ref = NULL;
+    error = git_reference_lookup(&upstream_ref, repo, full_remote_branch_name); 
+    if (error != 0) {
+        printf("Failed to lookup upstream reference.\n");
+        git_reference_free(local_branch_ref);
+        git_reference_free(head_ref);
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return 1;
+    }
+
+    const char *short_remote_branch_name = git_reference_shorthand(upstream_ref);
+
+    char remote_branch_name[128];
+    sprintf(remote_branch_name, "refs/remotes/%s", short_remote_branch_name);
+
+
+
+    git_reference *remote_branch_ref = NULL;
+    error = git_reference_lookup(&remote_branch_ref, repo, remote_branch_name);
+    if (error != 0) {
+        printf("Failed to lookup remote branch reference.\n");
+        git_reference_free(local_branch_ref);
+        git_reference_free(upstream_ref);
+        git_reference_free(head_ref);
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return 1;
+    }
+
+    const git_oid *remote_branch_commit_id = git_reference_target(remote_branch_ref);
+
+
+    int cmp_result = git_oid_cmp(local_branch_commit_id, remote_branch_commit_id);
+
+
+    printf("Local commit:  '%s'\tName: '%s'\n",
+           git_oid_tostr_s(local_branch_commit_id),
+           local_branch_name);
+    printf("Remote commit: '%s'\tName: '%s'\n",
+           git_oid_tostr_s(remote_branch_commit_id),
+           remote_branch_name);
+    printf("\n");
+
+
+    if (cmp_result != 0) {
+        printf("Local branch is different from the remote tracking branch.\n");
+    } else {
+        printf("Local branch is the same as the remote tracking branch.\n");
+    }
+
+    git_reference_free(local_branch_ref);
+    git_reference_free(remote_branch_ref);
+    git_reference_free(upstream_ref);
+    git_reference_free(head_ref);
+    git_repository_free(repo);
     git_libgit2_shutdown();
 
     return 0;
