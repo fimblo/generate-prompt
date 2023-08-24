f942bcf927759dc88f3e3aabbf7741852b51ab12
Mattias Jansson <fimblo@yanson.org>
2023-08-24 18:13:59 +0200

    fix: remove repeated code again

--- src/compare-git-refs.c
+++ src/compare-git-refs.c
@@ -36,7 +36,6 @@ int main() {
     char full_remote_branch_name[128];
     sprintf(full_remote_branch_name, "refs/remotes/origin/%s", git_reference_shorthand(head_ref));
 
-
     git_reference *upstream_ref = NULL;
     error = git_reference_lookup(&upstream_ref, repo, full_remote_branch_name); 
     if (error != 0) {
@@ -47,29 +46,14 @@ int main() {
         return 1;
     }
 
-    const char *short_remote_branch_name = git_reference_shorthand(upstream_ref);
-
     char remote_branch_name[128];
-    sprintf(remote_branch_name, "refs/remotes/%s", short_remote_branch_name);
+    sprintf(remote_branch_name, "refs/remotes/%s", git_reference_shorthand(upstream_ref));
+    const git_oid *remote_branch_commit_id = git_reference_target(upstream_ref);
 
 
-
-    git_reference *remote_branch_ref = NULL;
-    error = git_reference_lookup(&remote_branch_ref, repo, remote_branch_name);
-    if (error != 0) {
-        printf("Failed to lookup remote branch reference.\n");
-        git_reference_free(upstream_ref);
-        git_reference_free(head_ref);
-        git_repository_free(repo);
-        git_libgit2_shutdown();
-        return 1;
-    }
-
-    const git_oid *remote_branch_commit_id = git_reference_target(remote_branch_ref);
-
-
+    // compare the commit ids
+    
     int cmp_result = git_oid_cmp(local_branch_commit_id, remote_branch_commit_id);
-
 
     printf("Local commit:  '%s'\tName: '%s'\n",
            git_oid_tostr_s(local_branch_commit_id),
@@ -86,7 +70,6 @@ int main() {
         printf("Local branch is the same as the remote tracking branch.\n");
     }
 
-    git_reference_free(remote_branch_ref);
     git_reference_free(upstream_ref);
     git_reference_free(head_ref);
     git_repository_free(repo);
