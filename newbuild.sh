3b227e2256e0b742e476afc7244a1b7fce6dad7f
Mattias Jansson <fimblo@yanson.org>
2023-12-15 22:54:50 +0100

    ci: move binary to bin/

--- newbuild.sh
+++ newbuild.sh
@@ -3,8 +3,8 @@ rm build/*o
 echo Building...
 gcc -Wall -Wextra -c src/git-status.c -o build/git-status.o
 gcc -Wall -Wextra -c src/test-functions.c -o build/test-functions.o
-gcc build/git-status.o build/test-functions.o -o build/test-functions -lgit2
+gcc build/git-status.o build/test-functions.o -o bin/test-functions -lgit2
 echo run: build/test-functions
 echo -------------------------
-build/test-functions
+bin/test-functions
 
